62f6542e78c70e5245ed39842dd4e56c676eb061
bkantwi <bernardantwi2010@gmail.com>
2024-01-24 15:15:57 +0000

    more routes and error handling implemented

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,34 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    res.status(200).json({
+    res.status(201).json({
         message: 'Handling POST requests to products'
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id == 'special') {
+        res.status(200).json({
+            message: 'You discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product'
     });
 });
 
