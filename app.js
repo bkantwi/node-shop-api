7de8c9746490878a55cc0bc6e51def7dd96472fa
bkantwi <bernardantwi2010@gmail.com>
2024-01-25 20:01:53 +0000

    connection to mongodb atlas established.

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function () {
+  console.log('Connection to MongoDB Atlas Established!');
+});
+
+mongoose.connect(
+    'mongodb+srv://bkantwi:0r5A6PpZComH2ZG5@cluster0.79szhc1.mongodb.net/?retryWrites=true&w=majority'
+  );
+  
+
+// hot reload without manually restarting server
 app.use(morgan('dev'));
+
+// body parser for url encoded data and json data
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Handle cors errors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 // Routes which should handle requests
 app.use('/products', productRoutes);
