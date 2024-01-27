e85834a5000141a888bd0d9c5a53913ded9e67e4
bkantwi <bernardantwi2010@gmail.com>
2024-01-27 20:52:48 +0000

    still working on orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,16 +1,48 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
-const Order = require('../models/order'); 
+const Order = require('../models/order');
+const Product = require('../models/product'); 
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Oders were fetched'
+    Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(docs => {
+        res.status(200).json({
+            count: docs.length,
+            orders: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity,
+                    request: {
+                        type: 'GET',
+                        description: '',
+                        url: 'http://localhost:3001/orders/' + doc._id
+                    }
+                }
+            })
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
 router.post('/', (req, res, next) => {
+    Product.findById(req.body.productId)
+    .then(product => {
+
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Product not found',
+            error: err
+        });
+    });
     const order = new Order({
         _id: new mongoose.Types.ObjectId(),
         quantity: req.body.quantity,
@@ -20,7 +52,18 @@ router.post('/', (req, res, next) => {
     .save()
     .then(result => {
         console.log(result);
-        res.status(201).json(result);
+        res.status(201).json({
+            message: 'Order stored',
+            createOrder: {
+                _id: result._id,
+                product: result.product,
+                quantity: result.quantity
+            },
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3001/orders/' + result._id
+            }
+        });
     })
     .catch(err => {
         console.log(err);
